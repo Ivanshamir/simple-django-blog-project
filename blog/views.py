caf15f9685af64fbd6b41a369a13f853f024c22b
Ivanshamir <shamirimtiaz.ice@gmail.com>
2020-11-22 19:45:10 +0600

    Add CRUD functionality of post

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from . models import Post
+from django.urls import reverse_lazy
 
 class BlogListView(ListView):
     model = Post
@@ -12,3 +14,18 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
     context_object_name = 'anything_you_want'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    template_name = 'post_edit.html'
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
